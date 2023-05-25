97a0381cac08ae49c336fdbc4edee1022e33ea02
Nooc <nooc@nooc.ink>
2023-05-25 12:59:39 +0800

    Update page style

--- app/[...slug]/page.tsx
+++ app/[...slug]/page.tsx
@@ -51,8 +51,8 @@ export default async function PagePage({ params }: PageProps) {
 
   return (
     <article className="py-6 prose dark:prose-invert">
-      <h1>{page.title}</h1>
-      {page.description && <p className="text-md">{page.description}</p>}
+      <h1 className="mb-2">{page.title}</h1>
+      {page.description && <p className="text-md my-0">{page.description}</p>}
       <hr />
       <Mdx code={page.body.code} />
     </article>
