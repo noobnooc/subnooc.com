759b9133cc9666bd5f6431380e0c49315b52861d
Nooc <nooc@nooc.ink>
2023-05-30 16:51:24 +0800

    Add wechat card for posts

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -48,6 +48,9 @@ export const Post = defineDocumentType(() => ({
       type: "string",
       required: true,
     },
+    wechatLink: {
+      type: "string",
+    },
   },
   computedFields,
 }));
