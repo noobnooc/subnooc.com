954b68cd0d8f2050360a21cb03eb60fda0bc7091
Nooc <nooc@nooc.ink>
2023-06-02 17:03:45 +0800

    Add new post

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,22 @@
 import { allPosts } from "@/.contentlayer/generated";
 import { PostInfo } from "@/components/post-info";
 import Link from "next/link";
+import { compareDesc } from "date-fns";
 
 export default function Home() {
   return (
     <div className="prose dark:prose-invert">
-      {allPosts.map((post) => (
-        <article key={post._id}>
-          <Link href={post.slug}>
-            <h2>{post.title}</h2>
-          </Link>
-          {post.description && <p>{post.description}</p>}
-          <PostInfo className="-mt-2" post={post} />
-        </article>
-      ))}
+      {allPosts
+        .sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)))
+        .map((post) => (
+          <article key={post._id}>
+            <Link href={post.slug}>
+              <h2>{post.title}</h2>
+            </Link>
+            {post.description && <p>{post.description}</p>}
+            <PostInfo className="-mt-2" post={post} />
+          </article>
+        ))}
     </div>
   );
 }
