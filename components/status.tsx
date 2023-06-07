2d70cbe52f2bc2f467f74b8631ea06fde72eaddd
Nooc <nooc@nooc.ink>
2023-06-07 18:03:43 +0800

    Improve text

--- components/status.tsx
+++ components/status.tsx
@@ -83,7 +83,7 @@ export function Status({ className }: { className?: string }) {
             d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
           />
         </svg>
-        共 {prettifyNumber(status.totalVisits)} 次访问量
+        共 {prettifyNumber(status.totalVisits)} 次访问
       </div>
     </div>
   );
